4d118bf169fa48922ffdd40e1eab2b0638db3fdf
oscar4113 <65124475+oscar4113@users.noreply.github.com>
2026-01-09 12:15:21 +0000

    fix 1.1

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ echo installing web server
 sudo apt install nginx -y
 echo removing default files
 sudo rm -rf /var/www/html
+echo downloading new site
+git clone https://github.com/Joe-The-Chicken/polytrack polly/
 echo uploading new site
 sudo mv polly/ /var/www/html/
 echo site installed! 
